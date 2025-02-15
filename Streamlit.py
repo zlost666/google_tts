9bc1fc6a8a5638f9fc8b90ee347e87a46d0a07f8
zlost666 <max-k83@mail.ru>
2025-02-15 23:15:27 +0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,7 +3,7 @@ from google.cloud import texttospeech
 
 tts_credentials = st.secrets["google_tts"]["my_project_settings"]
 # Convert the AttrDict to a regular Python dictionary
-tts_credentials_dict = dict(my_project_settings)
+tts_credentials_dict = dict(tts_credentials)
 # Initialize Google Cloud TTS client with credentials
 client = texttospeech.TextToSpeechClient(credentials=tts_credentials_dict)
 st.write("Google Cloud TTS client initialized!")
